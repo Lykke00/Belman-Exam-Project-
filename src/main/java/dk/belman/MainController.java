5c976c415ff6d019ea0e4a260448511994231952
Lykke <lykber01@easv365.dk>
2025-04-12 20:09:54 +0200

    refactor af package navne

--- src/main/java/dk/belman/MainController.java
+++ src/main/java/dk/belman/MainController.java
@@ -1,9 +1,9 @@
-package dk.belman.belman;
+package dk.belman;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module dk.belman.belman {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens dk.belman.belman to javafx.fxml;
-    exports dk.belman.belman;
-}
