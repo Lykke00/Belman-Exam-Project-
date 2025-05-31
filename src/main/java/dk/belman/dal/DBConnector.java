2d973a15de2be7d446cb8a660603e4d51a64ba93
Lykke <lykber01@easv365.dk>
2025-05-31 20:26:51 +0200

    fikset database til ikke at være statisk mere

--- src/main/java/dk/belman/dal/DBConnector.java
+++ src/main/java/dk/belman/dal/DBConnector.java
@@ -10,20 +10,21 @@ import java.util.Properties;
 
 public class DBConnector implements IDBConnector {
     //designate file path for db settings
-    //private static final String PROPERTIES_FILE = "config/config.settings";
+    private static final String PROPERTIES_FILE = "config/config.settings";
     private final SQLServerDataSource dataSource;
 
     public DBConnector() throws IOException {
         //load db settings to properties
-    //    Properties props = new Properties();
-    //    props.load(new FileInputStream(PROPERTIES_FILE));
+        Properties props = new Properties();
+        props.load(new FileInputStream(PROPERTIES_FILE));
 
-        //dataSource configured with database connection details
+        // ændre disse til en statisk værdi hvis man vil bygge .apk til android
+        // github fatter ikke så meget af config filen (gitignore), da den også kører i et workflow
         dataSource = new SQLServerDataSource();
-        dataSource.setServerName("86.52.144.128");
-        dataSource.setDatabaseName("belsign");
-        dataSource.setUser("lykke");
-        dataSource.setPassword("elskerpatrick");
+        dataSource.setServerName(props.getProperty("Server"));
+        dataSource.setDatabaseName(props.getProperty("Database"));
+        dataSource.setUser(props.getProperty("User"));
+        dataSource.setPassword(props.getProperty("Password"));
         dataSource.setPortNumber(1433);
         dataSource.setTrustServerCertificate(true);
     }
